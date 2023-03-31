767f1a1b5a565dc91704115eb7bbed5748af5887
Eric Williams <eric.williams@vu.com>
2023-03-31 11:29:01 -0500

    Yo

--- displayInventory.py
+++ displayInventory.py
@@ -4,9 +4,11 @@ def displayInventory(inventory):
     # your code goes here
     total_items = 0
     print("Inventory:")
-    for item,total in stuff.items():
+    
+    for item,total in inventory.items():
         total_items = total_items + total
         print(total, item)
+    
     print("Total number of items:", total_items)
 
 if __name__ == "__main__":
