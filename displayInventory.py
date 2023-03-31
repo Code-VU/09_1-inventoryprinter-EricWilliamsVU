f796e233baa7fb82940a5e65493f16b39a9f9759
Eric Williams <eric.williams@vu.com>
2023-03-31 11:15:22 -0500

    How is this failing?

--- displayInventory.py
+++ displayInventory.py
@@ -2,7 +2,12 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
     # your code goes here
-
+    total_items = 0
+    print("Inventory:")
+    for item,total in stuff.items():
+        total_items = total_items + total
+        print(total, item)
+    print("Total number of items:", total_items)
 
 if __name__ == "__main__":
     displayInventory(stuff)
